d821cf72749fcccec77e4b0b63e0fe1c871a91e7
Rich Tabor <hi@richtabor.com>
2020-07-29 12:54:13 -0400

    Add core/heading stories

--- example/.blockbook/index.js
+++ example/.blockbook/index.js
@@ -58,6 +58,99 @@ registerTheme( {
 } );
 
 // Register BlockStories
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 1',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading one',
+				level: 1,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 2',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading two',
+				level: 2,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 3',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading three',
+				level: 3,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 4',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading four',
+				level: 4,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 5',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading five',
+				level: 5,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading Level 6',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading six',
+				level: 6,
+			},
+		},
+	],
+} );
+
+registerBlockStory( 'core/heading', {
+	name: 'Heading with colors',
+	blocks: [
+		{
+			name: 'core/heading',
+			attributes: {
+				content: 'Heading with colors',
+				level: 2,
+				backgroundColor: 'very-dark-gray',
+				textColor: 'white',
+			},
+		},
+	],
+} );
+
 registerBlockStory( 'core/buttons', {
 	name: 'Two Buttons',
 	blocks: [

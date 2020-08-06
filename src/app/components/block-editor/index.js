351e4a11284be3dd820dc16bfb969bbd62083db1
Fabian Kägy <mail@fabian-kaegy.de>
2020-08-06 12:49:33 +0200

    fix re add correct content in editor

--- src/app/components/block-editor/index.js
+++ src/app/components/block-editor/index.js
@@ -1,17 +1,19 @@
-import { getBlockFromExample } from '@wordpress/blocks';
-import { BlockPreview } from '@wordpress/block-editor';
+import { getBlockFromExample, createBlock } from '@wordpress/blocks';
+import { Editor } from '../editor';
 import './style.css';
 
 export function BlockEditor( { block } ) {
 	return (
-		<div className="bb-route-block__example">
-			<BlockPreview
-				viewportWidth={ 1000 }
-				blocks={ getBlockFromExample( block.name, {
-					attributes: block.example.attributes,
-					innerBlocks: block.example.innerBlocks,
-				} ) }
-			/>
-		</div>
+		<Editor
+			key={ block.name }
+			initialBlocks={ [
+				block.example
+					? getBlockFromExample( block.name, {
+							attributes: block.example.attributes,
+							innerBlocks: block.example.innerBlocks,
+					  } )
+					: createBlock( block.name ),
+			] }
+		/>
 	);
 }

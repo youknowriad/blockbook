9291861d6eef9aeb06262ac66a6ee0da8fa32d09
Fabian Kägy <mail@fabian-kaegy.de>
2020-08-11 11:49:37 +0200

    Revert "use actuall block markup for the storie previews"
    This reverts commit aef8da6b77896faf5ea47500653250bc07872014.

--- src/app/components/block-stories/index.js
+++ src/app/components/block-stories/index.js
@@ -1,26 +1,19 @@
-import { getBlockFromExample, serialize } from '@wordpress/blocks';
+import { getBlockFromExample } from '@wordpress/blocks';
+import { BlockPreview } from '@wordpress/block-editor';
 import './style.css';
 
 export function BlockStories( { stories } ) {
-	function getBlockMarkup( blockStory ) {
-		return serialize(
-			blockStory.blocks.map( ( childBlock ) =>
-				getBlockFromExample( childBlock.name, childBlock )
-			)
-		);
-	}
-
 	return (
 		<div className="bb-route-block__stories">
 			{ stories.map( ( blockStory ) => (
 				<div key={ blockStory.name }>
 					<h3>{ blockStory.name }</h3>
-					<div
-						className="bb-route-block__stories--preview"
-						dangerouslySetInnerHTML={ {
-							__html: getBlockMarkup( blockStory ),
-						} }
-					></div>
+					<BlockPreview
+						viewportWidth={ 1000 }
+						blocks={ blockStory.blocks.map( ( childBlock ) =>
+							getBlockFromExample( childBlock.name, childBlock )
+						) }
+					/>
 				</div>
 			) ) }
 		</div>

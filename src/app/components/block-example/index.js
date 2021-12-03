b0ee41eefce63d805ac0c9947e7b6aeefb2244e5
Riad Benguella <benguella@gmail.com>
2021-12-03 18:11:27 +0100

    Fix iframe previews (#60)

--- src/app/components/block-example/index.js
+++ src/app/components/block-example/index.js
@@ -1,17 +1,31 @@
 import { getBlockFromExample } from '@wordpress/blocks';
-import { BlockPreview } from '@wordpress/block-editor';
+import { BlockPreview, BlockEditorProvider } from '@wordpress/block-editor';
 import './style.css';
+import { getRegisteredThemes } from '../../api';
+import { useTheme } from '../../local-storage';
 
 export function BlockExample( { block } ) {
+	const [ currentThemeName ] = useTheme();
+	const currentTheme =
+		getRegisteredThemes().find(
+			( theme ) => theme.name === currentThemeName
+		) || getRegisteredThemes()[ 0 ];
+
 	return (
 		<div className="bb-route-block__example">
-			<BlockPreview
-				viewportWidth={ 1000 }
-				blocks={ getBlockFromExample( block.name, {
-					attributes: block.example.attributes,
-					innerBlocks: block.example.innerBlocks,
-				} ) }
-			/>
+			<BlockEditorProvider
+				settings={ {
+					styles: [ { css: currentTheme.editorStyles } ],
+				} }
+			>
+				<BlockPreview
+					viewportWidth={ 1000 }
+					blocks={ getBlockFromExample( block.name, {
+						attributes: block.example.attributes,
+						innerBlocks: block.example.innerBlocks,
+					} ) }
+				/>
+			</BlockEditorProvider>
 		</div>
 	);
 }

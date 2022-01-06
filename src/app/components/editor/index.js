69c9124746df5a3583ec7566bc7a02d15d3d6758
KJ Monahan <kj@forumone.com>
2022-01-06 09:23:31 -0600

    Add ShortcutProvider to Editor

--- src/app/components/editor/index.js
+++ src/app/components/editor/index.js
@@ -11,32 +11,35 @@ import {
 	DropZoneProvider,
 	Popover,
 } from '@wordpress/components';
+import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
 
 export function Editor( { initialBlocks } ) {
 	const [ blocks, setBlocks ] = useState( initialBlocks );
 	return (
 		<div className="editor-styles-wrapper">
-			<SlotFillProvider>
-				<DropZoneProvider>
-					<BlockEditorProvider
-						value={ blocks }
-						onChange={ setBlocks }
-						onInput={ setBlocks }
-						settings={ {
-							templateLock: 'all',
-						} }
-					>
-						<Popover.Slot name="block-toolbar" />
-						<BlockEditorKeyboardShortcuts />
-						<WritingFlow>
-							<ObserveTyping>
-								<BlockList />
-							</ObserveTyping>
-						</WritingFlow>
-					</BlockEditorProvider>
-				</DropZoneProvider>
-				<Popover.Slot />
-			</SlotFillProvider>
+			<ShortcutProvider>
+				<SlotFillProvider>
+					<DropZoneProvider>
+						<BlockEditorProvider
+							value={ blocks }
+							onChange={ setBlocks }
+							onInput={ setBlocks }
+							settings={ {
+								templateLock: 'all',
+							} }
+						>
+							<Popover.Slot name="block-toolbar" />
+							<BlockEditorKeyboardShortcuts />
+							<WritingFlow>
+								<ObserveTyping>
+									<BlockList />
+								</ObserveTyping>
+							</WritingFlow>
+						</BlockEditorProvider>
+					</DropZoneProvider>
+					<Popover.Slot />
+				</SlotFillProvider>
+			</ShortcutProvider>
 		</div>
 	);
 }
